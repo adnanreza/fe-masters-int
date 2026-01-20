e0bda5c1ac3fd1624ef37686e75d5d64dc6d7e12
Adnan Reza <adnan.reza@outlook.com>
2026-01-20 12:03:33 -0800

    Implement flattenArray

--- jsMethodPractice.js
+++ jsMethodPractice.js
@@ -1,4 +1,4 @@
-//Create a function that takes a string and 
+// Create a function that takes a string and 
 // returns a new str with duplicates removed
 
 function removeDuplicates(str){
@@ -12,3 +12,22 @@ function removeDuplicates(str){
 const str = 'This is is a test test string';
 console.log(str);
 console.log(removeDuplicates(str));
+
+
+// Without using .flat(), create a function to flatten an array
+
+function flatten(arr) {
+    return arr.reduce( (acc, item)=> {
+        if(Array.isArray(item)) {
+            acc = acc.concat(flatten(item));
+        } else {
+            acc.push(item);
+        }
+        console.log(acc);
+        return acc
+    }, [] )
+}
+
+const exampleArray = [1, 2, [3, 4, [5,6,7], 8], 9, 10]
+console.log('Before flattening: ' + exampleArray);
+console.log('After flattening: ' + flatten(exampleArray));
