8f115f4782da3c1747918eac24cd689d20281aa8
Adnan Reza <adnan.reza@outlook.com>
2026-01-20 14:21:35 -0800

    Implement debounce and throttle

--- jsMethodPractice.js
+++ jsMethodPractice.js
@@ -31,3 +31,54 @@ function flatten(arr) {
 const exampleArray = [1, 2, [3, 4, [5,6,7], 8], 9, 10]
 console.log('Before flattening: ' + exampleArray);
 console.log('After flattening: ' + flatten(exampleArray));
+
+// Implement debounce - imagine typing letter by leter into
+// a search box, and you don't want each keypress to trigger an API call
+// deboundedFn();
+// deboundedFn();
+// deboundedFn();
+
+function debounce(fn, time){
+    let setTimeoutID;
+
+    return function(){
+         if(setTimeoutID) clearTimeout(setTimeoutID);
+
+        setTimeoutID = setTimeout(()=>{
+            fn.apply(this, arguments);
+            setTimeoutID = null;
+        }, time);
+    }
+}
+
+function throttle(fn, time){
+    let setTimeoutID;
+
+    return function(){
+         if(setTimeoutID) return;
+
+        setTimeoutID = setTimeout(()=>{
+            fn.apply(this, arguments);
+            setTimeoutID = null;
+        }, time);
+    }
+} 
+
+//test debounce
+// Mock API call
+function mockApiCall(value) {
+  console.log('API call with:', value);
+}
+
+const debouncedSearch = debounce(mockApiCall, 1000);
+const throttledSearch = throttle(mockApiCall, 1000);
+
+const inputD = document.getElementById('searchInputD');
+const inputT = document.getElementById('searchInputT');
+
+inputD.addEventListener('input', (e) => {
+  debouncedSearch(e.target.value);
+});
+inputT.addEventListener('input', (e) => {
+  throttledSearch(e.target.value);
+});
